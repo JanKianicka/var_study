4b26a6a102c81c5ce2560e30d5971e256a45abaf
Jan Kianicka <kianicka@awst.at>
2017-12-06 16:10:42 +0100

    Clean out crap from wiki which did not work and initiate multithreading for multicore computation.

--- sungeom_c/sunrise_globe.c
+++ sungeom_c/sunrise_globe.c
@@ -11,6 +11,27 @@
 #include "stdio.h"
 #include <math.h>
 #include "sungeom.h"
+#include <pthread.h>
+
+
+typedef struct {
+    /* For running computation in multithreading mode we need 
+     * IN:
+     *   start_lon_idx - start longitude index
+     *   end_lon_idx   - end longitude index
+     *   longitude_array
+     *   latitude_array
+     *   year, month, day
+     * OUT:
+     *   sunrise_out_array 
+     */
+    int start_lon_idx;
+    int end_lon_idx;
+    void *Longitudes;
+    void *Latitudes;
+    void *Sun_rise_hours;
+
+} CONTEXT;
 
 inline double to_degrees(double radians) {
     return radians * (180.0 / M_PI);
@@ -18,6 +39,32 @@ inline double to_degrees(double radians) {
 
 inline double to_radians(double degrees){
     return (degrees * M_PI)/180.0;
+}
+
+CONTEXT *initiate_worker(int start_lon_idx, int end_lon_idx, void *Longitudes, void *Latitudes, void *Sun_rise_hours){
+    CONTEXT *context = malloc(sizeof(CONTEXT));
+    
+    context->start_lon_idx = start_lon_idx;
+    context->end_lon_idx   = end_lon_idx;
+    context->Longitudes    = Longitudes;
+    context->Latitudes     = Latitudes;
+    context->Sun_rise_hours = Sun_rise_hours;
+    return context;
+}
+
+void *sunrise_worker(void *context){
+    int ii=0;
+    CONTEXT *context_loc = (CONTEXT *) context;
+    int start_lon_idx = context_loc->start_lon_idx;
+    int end_lon_idx   = context_loc->end_lon_idx;
+    double (*Longitudes)[(long unsigned int)(ii)];
+    Longitudes = context_loc->Longitudes;
+
+
+    printf("st, en: %i, %i\n",start_lon_idx, end_lon_idx);
+    printf("%.2f\n",Longitudes[0][start_lon_idx+2]);
+//    printf("%d\n",Longitudes);*/
+    
 }
 
 
@@ -28,7 +75,8 @@ int main(int argv, char *argc[])
     int jdn;
     n = 180;
     m = 360;
-    double res = 0.05;
+    double res = 1; /* 0.05; */
+    int nThreads = 3;
     int lat_size, lon_size;
     lat_size = n/res;
     lon_size = m/res;
@@ -45,30 +93,11 @@ int main(int argv, char *argc[])
     int year = 2017;
     int month = 12;
     int day = 5;
-    double num_of_days;
-/*    double mean_solar_time;
-    double solar_mean_anomaly;
-    double equation_of_center;
-    double ecplitic_longitude;
-    double solar_transit;
-    double declination_of_sun;
-    double hour_angle;
-    double jd_rise;
-    double jd_set; */
     double sunrise_new;
     
     printf("lat_size: %i\n",lat_size);
     printf("lon_size: %i\n",lon_size);
-    
-    /* Calculation of Julian day for the given date */
-    /* Incorect formula from wikipage, could be that also sunrise equiations are wrong
-       jdn = 367*year - (7*(year+5001 + (month - 9)/7))/4 + (275*month)/9 + day + 1729777; */
-    /* Correct formula from http://aa.usno.navy.mil/faq/docs/JD_Formula.php */
-    jdn= day-32075+1461*(year+4800+(month-14)/12)/4+367*(month-2-(month-14)/12*12)/12-3*((year+4900+(month-14)/12)/100)/4;
-    printf("jdn: %i\n",jdn);
-    num_of_days = (double)jdn - 2451545.0 + 0.0008;
-    printf("num_of_days: %.2f\n",num_of_days);
-    
+  
     for(i=0;i<lat_size;i++){
 	i_d = (double)i;
 	loc_latitude = i_d*res - 90.0;
@@ -78,50 +107,12 @@ int main(int argv, char *argc[])
 	    Longitudes[i][j] = loc_longitude;
 	    Latitudes[i][j] = loc_latitude;
 	}
-//	printf("%.2f\n",Longitudes[i][190]);
-	printf("%.2f\n",Latitudes[i][0]);
     }
 
     /* Calculation of sunrise itself*/
     /* lat_size - runs in latitudes, lon_size - runs in longitudes */
     for(i=0;i<lat_size;i++){
 	for(j=0;j<lon_size;j++){
-	    /* all of these equations are from wiki page which seems to be crappy 
-	       unfortunatelly. Now we can see how tricky is to rely on internet resources. */
-            /*   
-	    mean_solar_time = num_of_days - (Longitudes[i][j]/360.0);
-
-	    solar_mean_anomaly = fmod((357.5291 + 0.98560028*mean_solar_time), 360.0);
-	    // equation from https://github.com/maoserr/redshiftgui/blob/master/src/solar.c
-	    // solar_mean_anomaly = (357.5291 + mean_solar_time*(35999.05029 - mean_solar_time*0.0001537));
-	    equation_of_center = 1.9148*sin(solar_mean_anomaly) + 0.0200*sin(2*solar_mean_anomaly) + 0.0003*sin(3*solar_mean_anomaly);
-	    ecplitic_longitude = fmod((solar_mean_anomaly + equation_of_center + 180.0 + 102.9372),360.0);
-	    solar_transit = 2451545.5 + mean_solar_time + 0.0053*sin(solar_mean_anomaly) - 0.0069*sin(2*ecplitic_longitude);
-	    declination_of_sun = asin(sin(ecplitic_longitude)*(sin(to_radians(23.44))) );
-	    hour_angle = acos((sin(to_radians(-0.83)) - (sin(to_radians(Latitudes[i][j])) * sin(declination_of_sun))) / (cos(to_radians(Latitudes[i][j])) * cos(declination_of_sun)));
-	    jd_rise = solar_transit - (hour_angle/to_radians(360.0));
-	    jd_set  = solar_transit + (hour_angle/to_radians(360.0)); */
-
-#if DEBUG	    
-	    printf("%.2f\n",mean_solar_time);
-	    printf("%.2f\n",solar_mean_anomaly);
-	    printf("%.2f\n",equation_of_center);
-	    printf("%.2f\n",ecplitic_longitude);
-	    printf("%.2f\n",solar_transit);
-	    printf("%.2f\n",declination_of_sun);
-	    printf("ha: %.2f\n",hour_angle);
-	    printf("Lat: %.2f\n",Latitudes[i][j]);
-	    printf("jd_rise: %.2f\n",jd_rise);
-	    printf("jd_set:%.2f\n",jd_set); */
-	    if(Longitudes[i][j] == 17){
-		printf("Lat: %.2f\n",Latitudes[i][j]);
-		printf("jd_rise: %.2f\n",jd_rise);
-		printf("jd_set:%.2f\n",jd_set);
-		}
-
-	    hour_angle = solar_hour_angle_from_time(solar_transit, Latitudes[i][j], Longitudes[i][j]);
-	    printf("ha new: %.2f\n",hour_angle);
-#endif
 	    sunrise_new = calculateSunrise(year, month, day, Latitudes[i][j], Longitudes[i][j], 0, 0);
 	    if(Longitudes[i][j] == 0){
 		printf("Lat: %.2f\n",Latitudes[i][j]);
@@ -136,7 +127,38 @@ int main(int argv, char *argc[])
  */
 	}
     }
-	
+
+    /* Using multithreading */
+    /* Initialize context */
+    CONTEXT **context_arr = malloc(sizeof(CONTEXT)*nThreads);
+    int increment = lon_size/nThreads;
+    printf("increment:%i\n",increment);
+    int start_lon_idx = 0;
+    int end_lon_idx   = 0;
+    pthread_t *pthreads = malloc(sizeof(pthread_t)*nThreads);
+    
+    for(i=0; i<nThreads;i++){
+	end_lon_idx = start_lon_idx + increment;
+	if(end_lon_idx>lon_size)
+	    end_lon_idx = lon_size;
+	printf("end_lon_idx: %i, %i \n",start_lon_idx,end_lon_idx);
+	context_arr[i] = initiate_worker(start_lon_idx, end_lon_idx, Longitudes, Latitudes, Sun_rise_hours);
+	pthread_create(&pthreads[i], NULL, sunrise_worker, (void *) context_arr[i]);
+	start_lon_idx = end_lon_idx + 1;
+    }
+
+    for(i=0; i<nThreads;i++){
+	pthread_join(pthreads[i], NULL);
+    }
+    
+    for(i=0; i<nThreads;i++){
+	free(context_arr[i]);
+    }
+    
+    free(context_arr);
+    free(pthreads);
+
+
     free(Longitudes);
     free(Latitudes);
     free(Sun_rise_hours);
