37dc742822d17b8e2dba13db70c7ba13b9b11b37
Jan Kianicka <jan.kianicka@ctbto.org>
2019-11-29 15:40:29 +0100

    Bash study - array variables and started with operators.

--- bash/README.txt
+++ bash/README.txt
@@ -12,8 +12,48 @@ Two basic types of shell:
     default prompt.
     	    Subcategories: C shell (csh), TENEX/TOPS C shell (tcsh)
 
-Variabvle names:
+Variable names:
 - upper case, must not contain number,!*-
 There are three types of variables: local, environmental, shell
 
+Command line arguments - try:
+$ ./basic_bash_commands.sh arg1 arg2 arg3 arg4
+
+The $? variable represents the exit status of the previous command.
+
+Shell Arrays:
+
+bash operators:
+Bourne shell didn't originally have any mechanism to perform simple arithmetic operations but it uses external programs, either awk or expr.
+`expr $a + $b` will give 30
+`expr $a - $b` will give -10
+`expr $a \* $b` will give 200
+`expr $b / $a` will give 2 
+`expr $b % $a` will give 0 - Modulus
+a = $b would assign value of b into a
+[ $a == $b ] would return false.
+[ $a != $b ] would return true.
+
+spaces around them, for example [ $a == $b ] is correct whereas, [$a==$b] is incorrect.
+
+Numerical relational operators:
+-eq - equals:     [ $a -eq $b ] is not true.
+-ne - not equals: [ $a -ne $b ] is true.
+-gt - greater:    [ $a -gt $b ] is not true.
+-lt - less:       [ $a -lt $b ] is true.
+-ge - greater or equal:  [ $a -ge $b ] is not true.
+-le - less or equal:     [ $a -le $b ] is true.
+spaces around them. For example, [ $a <= $b ] is correct whereas, [$a <= $b] is incorrect.
+
+Boolean operators:
+! - logical negation: [ ! false ] is true.
+-o - logical OR: [ $a -lt 20 -o $b -gt 100 ] is true.
+-a - logical AND: [ $a -lt 20 -a $b -gt 100 ] is false.
+
+String operators:
+= - equality:      [ $a = $b ] is not true.
+!= - non equliaty: [ $a != $b ] is true.
+-z - size is zero: [ -z $a ] is not true.
+-n - size is non-zero: [ -n $a ] is not false.
+str - str is not the empty string: [ $a ] is not false.
 
--- bash/basic_bash_commands.sh
+++ bash/basic_bash_commands.sh
@@ -31,3 +31,92 @@ echo "PID of the current shell: $$"
 echo "File name of the current script: $0"
 echo "Number of arguments: $#"
 echo "Arguments: $*"
+
+# Special Parameters $* and $@
+echo "Processing arguments using \$*:" 
+for TOKEN in $*
+do
+   echo $TOKEN
+done
+
+echo "Processing arguments using \$@:"
+for TOKEN in $@
+do
+   echo $TOKEN
+done
+# error invocation
+touch /root/xxx
+echo "Exist status of the last command: $?"
+
+echo "Shell Arrays" 
+NAME01="Zara"
+NAME02="Qadir"
+NAME03="Mahnaz"
+NAME04="Ayan"
+NAME05="Daisy"
+# can be stored as
+NAME[0]="Zara"
+NAME[1]="Qadir"
+NAME[2]="Mahnaz"
+NAME[3]="Ayan"
+NAME[4]="Daisy"
+echo "First element of the array: $NAME"
+echo "First element of the array using {}: ${NAME[0]} "
+# elements can be accessed via [*] or [@]
+for name in ${NAME[*]}
+do
+    echo $name
+done
+
+NAME2=('value1' 'value2' 'value3')
+# in csh - % set -A array_name value1 value2 ... valuen
+echo "All elements of NAME2 at once: ${NAME2[@]}"
+
+# Bourne shell basic operators
+# originally we have to use awk or expr 
+# there must be spaces: 2 + 2 (not 2+2)
+# enclosed between ‘ ‘, called the backtick.
+val=`expr 2 + 2`
+echo "Total value : $val"
+
+asign_a=23
+asign_b=$asign_a
+echo [ $asign_a == $asign_b ]
+if [ $asign_a == $asign_b ]
+then 
+   echo "asign_a equals to asign_b"
+fi
+
+# spaces around them. For example, [ $a <= $b ] is correct whereas, [$a <= $b] is incorrect.
+if [ $asign_a -eq $asign_b ]
+then 
+    echo "asign_a equals to asign_b using -eq"
+fi
+
+# boolean operators
+if [ !false ]
+then
+    echo "!false is true"
+    if [ $asign_a -gt 20 -o $asign_b -lt 20 ]
+    then
+	echo "OR evaluation of $asign_a -gt 20 -o $asign_b -lt 20"
+	if [ $asign_a -gt 20 -a $asign_b -ge 23 ]
+	then
+	    echo "AND evaluation of $asign_a -gt 20 -a $asign_b -ge 23"
+	fi
+    fi
+fi
+
+string_a='abc'
+string_b='efg'
+
+if [ $string_a != $string_b ]
+then
+    echo "String equlity: $string_a = $string_b not true."
+fi
+
+# The same with negation outside the condition
+if ! [[ $string_a = $string_b ]]
+then
+    echo "String equlity: ! [[\$string_a = \$string_b]] not true."
+fi
