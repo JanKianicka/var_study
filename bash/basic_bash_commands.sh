3d2c51ae9d97b8a17b732b4bf2e2ed488649a269
Jan Kianicka <jan.kianicka@ctbto.org>
2020-03-02 09:47:41 +0100

    Some more bash study - loops, testing files.

--- bash/basic_bash_commands.sh
+++ bash/basic_bash_commands.sh
@@ -120,3 +120,75 @@ if ! [[ $string_a = $string_b ]]
 then
     echo "String equlity: ! [[\$string_a = \$string_b]] not true."
 fi
+
+# Testing of files
+if [ -b /dev/sda ]; then echo "/dev/sda is block special file"; fi
+if [ -c /dev/null ]; then echo "/dev/null is character special file"; fi
+
+# Interesting if..elif..else chain
+a=10
+b=20
+if [ $a == $b ]
+then
+   echo "a is equal to b"
+elif [ $a -gt $b ]
+then
+   echo "a is greater than b"
+elif [ $a -lt $b ]
+then
+   echo "a is less than b"
+else
+   echo "None of the condition met"
+fi
+
+# case..asac 
+FRUIT="kiwi"
+case "$FRUIT" in
+   "apple") echo "Apple pie is quite tasty." 
+   ;;
+   "banana") echo "I like banana nut bread." 
+   ;;
+   "kiwi") echo "New Zealand is famous for kiwi." 
+   ;;
+esac
+
+# Usage for parsing command line options
+# option="${1}" 
+# case ${option} in 
+#   -f) FILE="${2}" 
+#      echo "File name is $FILE"
+#      ;; 
+#   -d) DIR="${2}" 
+#      echo "Dir name is $DIR"
+#      ;; 
+#   *)  
+#      echo "`basename ${0}`:usage: [-f file] | [-d directory]" 
+#      exit 1 # Command to come out of the program with status 1
+#      ;; 
+# esac 
+
+
+# Nested while loop
+a=0
+while [ "$a" -lt 10 ]    # this is loop1
+do
+   b="$a"
+   while [ "$b" -ge 0 ]  # this is loop2
+   do
+      echo -n "$b "
+      b=`expr $b - 1`
+   done
+   echo
+   a=`expr $a + 1`
+done
+
+# for loop - space separated list in the input
+for var in 0 1 2 3 4 5 6 7 8 9
+do
+   echo $var
+done
+
+for FILE in $HOME/*
+do
+   echo $FILE
+done
