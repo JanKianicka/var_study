468ccbe5b6dd5db81ea769984190860ec003aa15
Jan Kianicka <jan.kianicka@geology.sk>
2025-01-23 14:06:09 +0100

    Implements first version of downloading from OData for S1 based on oribit attributes and dates

--- python_tests/CDSE_download/download_S1.py
+++ python_tests/CDSE_download/download_S1.py
@@ -1,9 +1,33 @@
+'''
+    download_S1.py is the script for downloading 
+    primary Sentinel1 data from OData Copernicus Data Service.
+    It is designed to download based on 
+    - Collection name
+    - contains attribute in the product name which filters the level of processing
+    - orbit direction
+    - relative orbit number
+    - slice number
+    - data start, date end
+    
+    Initial version:
+        Jan Kianicka, January 2025
+    TODO:
+    - check env. variables for credentials
+    - handle better exceptions
+    - check md5 hash codes 
+    - retry in case of failure
+    - fill in missing data
+    - add ordering in the query
+     
+'''
 import configparser
 import os
+import requests
 import sys
+import time
 
 # CONSTANTS
-ODATA_SERVICE_URL = '"https://catalogue.dataspace.copernicus.eu/odata/v1/Products'
+ODATA_SERVICE_URL = 'https://catalogue.dataspace.copernicus.eu/odata/v1/Products'
 FILTER_COLLECTION_NAME = 'Collection/Name eq %s'
 FILTER_CONTAINS_NAME = 'contains(Name,%s)'
 FILTER_ATT_ORBIT_DIRECTION = 'Attributes/OData.CSC.StringAttribute/any(att:att/Name eq \'orbitDirection\' and att/OData.CSC.StringAttribute/Value eq %s)'
@@ -11,6 +35,9 @@ FILTER_ATT_REL_ORBIT_NUM = 'Attributes/OData.CSC.IntegerAttribute/any(att:att/Na
 FILTER_ATT_SLICE_NUM = 'Attributes/OData.CSC.IntegerAttribute/any(att:att/Name eq \'sliceNumber\' and att/OData.CSC.IntegerAttribute/Value eq %s)'
 FILTER_ATT_CONT_DATE_START = 'ContentDate/Start gt %s'
 FILTER_ATT_CONT_DATE_END = 'ContentDate/Start lt %s'
+
+ODATA_SERVICE_TOKEN_URL = 'https://identity.dataspace.copernicus.eu/auth/realms/CDSE/protocol/openid-connect/token'
+ODATA_DOWNLOAD_SERVICE_URL = 'https://download.dataspace.copernicus.eu/odata/v1/Products(%s)/$value'
 
 copernicus_user = os.getenv("copernicus_user") # copernicus User
 copernicus_password = os.getenv("copernicus_password") # copernicus Password
@@ -50,9 +77,64 @@ for key in config['S1 donwload pars']:
     elif key == 'cont_date_end':
         filter_cont_date_end = (FILTER_ATT_CONT_DATE_END%config['S1 donwload pars'][key])
         ODATA_QUERY = ODATA_QUERY + ' and ' + filter_cont_date_end
+    elif key == 'target_dir':
+        TARGET_BASE_DIR = config['S1 donwload pars'][key]
     else:
         print("Not parsed config key", key)
+
+# Connection token
+post_data = {
+    "client_id": "cdse-public",
+    "username": copernicus_user,
+    "password": copernicus_password,
+    "grant_type": "password",
+}
+r = requests.post(
+            ODATA_SERVICE_TOKEN_URL,
+            data = post_data
+        )
+
+token = r.json()['access_token']
+  
+print("Search data query: ", ODATA_QUERY)
+json = requests.get(ODATA_QUERY).json()
+productList = json['value']
+
+print("Generated authorization token with this start", token[:10])
+print("Number of found scenes to download: ", productList.__len__())
+for scene in productList:
+    sceneName = scene.get('Name')
+    sceneId = scene.get('Id')
+    download_URL = (ODATA_DOWNLOAD_SERVICE_URL%sceneId)
+    print('Small break')
+    time.sleep(30)
     
-print(ODATA_QUERY)
+    print('Starting downloading product: ', sceneName)
+    print(download_URL)
+    
+    start = time.time()
+    
+    headers = {"Authorization": f"Bearer {token}"}
 
+    # Create a session and update headers
+    session = requests.Session()
+    session.headers.update(headers)
 
+    # Perform the GET request
+    response = session.get(download_URL, stream=True)
+
+    # Check if the request was successful
+    if response.status_code == 200:
+        with open(os.path.join(TARGET_BASE_DIR.strip('\''), sceneName.replace('SAFE', 'zip')), "wb") as file:
+            for chunk in response.iter_content(chunk_size=8192):
+                if chunk:  # filter out keep-alive new chunks
+                    file.write(chunk)
+    else:
+        print(f"Failed to download file. Status code: {response.status_code}")
+        print(response.text)
+    
+    print(sceneName + " successfully downloaded")
+    session.close()
+    end = time.time()
+    print('Elapsed time', '%.2f'%((end - start)/60) + ' min')
+    
